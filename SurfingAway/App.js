d984c17e54ead9334b3a7cf9e5d9c5873bef1f58
Elsa Catoire <elsa.catoire@gmail.com>
2023-08-31 15:23:52 +0200

    navbar unfix too hight

--- SurfingAway/App.js
+++ SurfingAway/App.js
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import AddSpot from './src/screens/AddSpot';
 import Home from './src/screens/Home';
-import SpotPage from './src/screens/SpotPage';
 import Weather from './src/screens/Weather';
 import Spots from './src/screens/Spots';
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,13 +11,16 @@ const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
+
     <NavigationContainer style={styles.top} >
       <Tab.Navigator>
+        <Tab.Screen name={'Home'} component={Home} />
         <Tab.Screen name={'Spots'} component={Spots} />
         <Tab.Screen name={'Weather'} component={Weather} />
         <Tab.Screen name={'Add'} component={AddSpot} />
       </Tab.Navigator>
     </NavigationContainer>
+
   );
 }
 
