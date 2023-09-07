46275f766915cd84da43866947f60d6d339ee033
AlissiaLR <alissia090501@gmail.com>
2023-09-07 14:22:51 +0200

    bug affichage

--- SurfingAway/src/components/UpcommingWeather.js
+++ SurfingAway/src/components/UpcommingWeather.js
@@ -2,14 +2,55 @@ import React from "react";
 import { View, Text, StyleSheet, FlatList, SafeAreaView } from "react-native";
 import { Feather } from '@expo/vector-icons'
 
-async function imageApi() {
-    let city = prompt("Vous voulez voir la météo de quelle ville ?")
-    let URL = "https://api.openweathermap.org/data/2.5/weather?appid={d405fb101d33dad60e6deb3993ca48a7}" + "&q=" + encodeURIComponent(city);
-    const url = await fetch(URL);
-    const xml = await url.xml();
-    return xml
+const DATA = {
+    "coord": {
+        "lon": 10.99,
+        "lat": 44.34
+    },
+    "weather": [
+        {
+            "id": 501,
+            "main": "Rain",
+            "description": "moderate rain",
+            "icon": "10d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 298.48,
+        "feels_like": 298.74,
+        "temp_min": 297.56,
+        "temp_max": 300.05,
+        "pressure": 1015,
+        "humidity": 64,
+        "sea_level": 1015,
+        "grnd_level": 933
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 0.62,
+        "deg": 349,
+        "gust": 1.18
+    },
+    "rain": {
+        "1h": 3.16
+    },
+    "clouds": {
+        "all": 100
+    },
+    "dt": 1661870592,
+    "sys": {
+        "type": 2,
+        "id": 2075663,
+        "country": "IT",
+        "sunrise": 1661834187,
+        "sunset": 1661882248
+    },
+    "timezone": 7200,
+    "id": 3163858,
+    "name": "Zocca",
+    "cod": 200
 }
-imageApi()
 
 const Item = (props) => {
     const { min, max } = props
@@ -27,7 +68,8 @@ const Item = (props) => {
                 <Text>Vent : 21 km/h</Text>
             </View>
             <View style={styles.feels}>
-                <Text>{min}°/{max}°</Text>
+                <Text>{min}°</Text>
+                <Text>{max}°</Text>
                 <Text>Feels : 22°</Text>
             </View>
             <View style={styles.days}>
