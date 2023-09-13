2f295e6c15fa506903168f649ee8ec2c93a7d4ab
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-09-13 15:33:55 +0200

    CSS fix

--- SurfingAway/src/components/MainTitle.js
+++ SurfingAway/src/components/MainTitle.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-import { Text } from 'react-native-paper';
+import { Text } from "react-native-paper";
 
 const MainTitle = (props) => {
-    const { titleText } = props
-    return (
-        <View style={styles.titleWrapper} >
-            <Text style={styles.textEmphase} >{titleText}</Text>
-        </View>
-    )
-}
+  const { titleText } = props;
+  return (
+    <View style={styles.titleWrapper}>
+      <Text style={styles.textEmphase}>{titleText}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    titleWrapper: {
-        backgroundColor: 'deeppink',
-        alignItems: 'center',
-    },
-    textEmphase: {
-        fontWeight: 'bold',
-        fontSize: 30,
-        color: 'darkblue',
-        paddingVertical: 5,
-    },
-})
+  titleWrapper: {
+    backgroundColor: "deeppink",
+    alignItems: "center",
+  },
+  textEmphase: {
+    fontWeight: "bold",
+    fontSize: 25,
+    color: "white",
+    paddingVertical: 10,
+  },
+});
 
-export default MainTitle
+export default MainTitle;
