8f20d4836e381df8c5f57670ecaa8e1aaf34653d
Elsa Catoire <elsa.catoire@gmail.com>
2023-09-14 16:40:14 +0200

    moment lib for time conversion add info weather

--- SurfingAway/src/components/CardWeather.js
+++ SurfingAway/src/components/CardWeather.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Card, Button, Title, Paragraph, Avatar } from 'react-native-paper';
 import getWeatherSpot from "../services/getWeatherSpot";
 
@@ -12,6 +12,7 @@ const CardWeather = (props) => {
     useEffect(() => {
         const fetchDataWeather = async () => {
             try {
+                // dataWeatherArray = [temp_min, temp_max, windSpeed, weatherDescription, pressure, humidity, sunrise, sunset]
                 const weather = await getWeatherSpot(lat, lon);
                 setWeatherData(weather)
                 let weatherStr = weather[3]
@@ -25,14 +26,19 @@ const CardWeather = (props) => {
     }, []);
     return (
         <Card>
-            <Card.Content>
-                <Title style={styles.title} >Météo du jour</Title>
-                <Text style={styles.weatherInfos} >{weatherDescription}</Text>
-                <Paragraph style={styles.weatherInfos} >Vitesse du vent : {weatherData[2] + "km/h"}</Paragraph>
-                <Paragraph style={styles.weatherInfos} >Pression : { }</Paragraph>
-            </Card.Content>
-            <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-            <Card.Actions>
+            <View style={styles.wetaherWrap}>
+                <Card.Content style={styles.cardContent} >
+                    <Title style={styles.title} >Météo du spot</Title>
+                    <Text style={styles.weatherInfos} >{weatherDescription}</Text>
+                    <Text style={styles.weatherInfos} >Vitesse du vent : {weatherData[2] + "km/h"}</Text>
+                    <Text style={styles.weatherInfos} >Pression : {weatherData[4]}</Text>
+                    <Text style={styles.weatherInfos} >Humidité : {weatherData[5]}</Text>
+                    <Text style={styles.weatherInfos} >Lever de soleil : {weatherData[6]}</Text>
+                    <Text style={styles.weatherInfos} >Coucher du soleil : {weatherData[7]}</Text>
+                </Card.Content>
+                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} style={styles.stretch} />
+            </View>
+            <Card.Actions >
                 <Button style={styles.buttonCard}>{Math.round(weatherData[0]) + "°C"}</Button>
                 <Button style={styles.buttonCard}>{Math.round(weatherData[1]) + "°C"}</Button>
             </Card.Actions>
@@ -44,11 +50,25 @@ const styles = StyleSheet.create({
         paddingHorizontal: 1,
     },
     title: {
-        fontSize: 30
+        fontSize: 20,
+        paddingVertical: 1,
+        fontWeight: "bold"
+    },
+    wetaherWrap: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    stretch: {
+        width: "35%",
+        height: 200,
+        resizeMode: 'stretch',
+        paddingRight: 5,
     },
     weatherInfos: {
-        fontSize: 20,
-        paddingVertical: 3
+        fontSize: 16,
+        paddingVertical: 2
     },
 })
 
--- SurfingAway/src/services/getWeatherSpot.js
+++ SurfingAway/src/services/getWeatherSpot.js
@@ -1,18 +1,6 @@
 import React from "react";
 import { WEATHER_API_KEY } from "@env"
-
-function msToTime(duration) {
-    var milliseconds = Math.floor((duration % 1000) / 100),
-        seconds = Math.floor((duration / 1000) % 60),
-        minutes = Math.floor((duration / (1000 * 60)) % 60),
-        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
-}
+import moment from "moment"
 
 const getWeatherSpot = async (lat, lon) => {
     try {
@@ -22,7 +10,6 @@ const getWeatherSpot = async (lat, lon) => {
         const data = await res.json()
         let arrayDataWeather = []
         const dataMain = data.list[0]
-        console.log(msToTime(data.city.sunrise));
         console.log('fetch weather successfull');
         // dataWeatherArray = [temp_min, temp_max, windSpeed, weatherDescription, pressure, humidity, sunrise, sunset]
         arrayDataWeather.push(dataMain.main['temp_min'])
@@ -31,8 +18,8 @@ const getWeatherSpot = async (lat, lon) => {
         arrayDataWeather.push(dataMain.weather[0].description)
         arrayDataWeather.push(dataMain.main['pressure'])
         arrayDataWeather.push(dataMain.main['humidity'])
-        arrayDataWeather.push(dataMain.main['humidity'])
-        arrayDataWeather.push(dataMain.main['humidity'])
+        arrayDataWeather.push(moment(data.city.sunrise).format("HH:mm:ss"))
+        arrayDataWeather.push(moment(data.city.sunset).format("HH:mm:ss"))
 
         console.log(arrayDataWeather);
         return arrayDataWeather
