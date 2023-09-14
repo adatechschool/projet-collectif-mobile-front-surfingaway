7870c0c2886623a9bfd61a859a76ef2133609d0f
Agathe Szkola <agathe.szkola@gmail.com>
2023-09-14 15:31:20 +0200

    création carte + affichage des spots + utilisation localisation pour afficher position de départ carte

--- SurfingAway/src/components/HomeTopTabs.js
+++ SurfingAway/src/components/HomeTopTabs.js
@@ -2,34 +2,32 @@ import React from "react";
 import AddSpot from "../screens/AddSpot";
 import Home from "../screens/Home";
 import Spots from "../screens/Spots";
+import Map from "../screens/Map";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-/* const heightStatus = Platform.OS === 'android' ? StatusBar.currentHeight : 24; */
-
 const HomeTopTabs = () => {
-    const Tab = createMaterialTopTabNavigator();
-    return (
+  const Tab = createMaterialTopTabNavigator();
+  return (
 
-        <Tab.Navigator
-            /* style={{ marginTop: heightStatus }} */
-            screenOptions={{
-                tabBarActiveTintColor: 'deeppink',
-                tabBarInactiveTintColor: 'aliceblue',
-                tabBarStyle: {
-                    backgroundColor: 'darkblue',
-                },
-                headerStyle: {
-                    backgroundColor: 'darkblue '
-                },
-            }}
-        >
-            <Tab.Screen name={'Actu'} component={Home} />
-            <Tab.Screen name={'Spots'} component={Spots} />
-            {/*             <Tab.Screen name={'Météo'} component={Weather} /> */}
-            <Tab.Screen name={"+"} component={AddSpot} />
-        </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'deeppink',
+        tabBarInactiveTintColor: 'aliceblue',
+        tabBarStyle: {
+          backgroundColor: 'darkblue',
+        },
+        headerStyle: {
+          backgroundColor: 'darkblue '
+        },
+      }}
+    >
+      <Tab.Screen name={'Actu'} component={Home} />
+      <Tab.Screen name={'Spots'} component={Spots} />
+      <Tab.Screen name={'Carte'} component={Map} />
+      <Tab.Screen name={"+"} component={AddSpot} />
+    </Tab.Navigator>
 
-    );
+  );
 }
 
 export default HomeTopTabs
---
+++ SurfingAway/src/screens/Map.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
+import getAllSpots from "../services/getAllSpots";
+import * as Location from "expo-location";
+
+const Map = () => {
+  const [mapLat, setMapLat] = useState(0);
+  const [mapLong, setMapLong] = useState(0);
+  const [spotsCoords, setSpotsCoords] = useState([]);
+  const [error, setError] = useState(null);
+
+  const getAllCoords = async () => {
+    try {
+      const records = await getAllSpots();
+      let coordsArray = [];
+
+      records.forEach(spot => {
+        coordsArray.push({
+          latitude: spot.fields.Latitude,
+          longitude: spot.fields.Longitude,
+          name: spot.fields.Address
+        })
+      });
+      setSpotsCoords(coordsArray);
+
+    } catch (error) {
+      setError('Could not fetch weather');
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setMapLat(location.coords.latitude);
+      setMapLong(location.coords.longitude);
+      await getAllCoords();
+    })()
+  }, [])
+
+  return (
+    <View style={styles.container} >
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: mapLat,
+          longitude: mapLong,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+      >
+        {spotsCoords.map((data, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: parseFloat(data.latitude),
+              longitude: parseFloat(data.longitude),
+            }}
+            title={data.name}
+          />
+        ))}
+      </MapView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  }
+})
+
+export default Map;
