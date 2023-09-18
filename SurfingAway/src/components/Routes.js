ea337996b3fa9fe0b483eeac0b7160fc05db9e34
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-09-18 16:58:29 +0200

    Finalisation routage

--- SurfingAway/src/components/Routes.js
+++ SurfingAway/src/components/Routes.js
@@ -6,15 +6,33 @@ import Home from "../screens/Home";
 import HomeTopTabs from "./HomeTopTabs";
 import Map from "../screens/Map";
 import LoginPage from "../screens/LoginPage";
+import SignUpPage from "../screens/SignUpPage";
 
 const Stack = createNativeStackNavigator();
 
 const Routes = () => {
+  //  // Définition de l'état d'authentification initial à false
+  //  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  //  useEffect(() => {
+  //    // Ici, vous pouvez placer la logique de vérification de l'authentification
+  //    // Par exemple, vous pouvez vérifier si l'utilisateur est connecté
+  //    // en utilisant une méthode d'authentification (Firebase, API, etc.).
+  //    // Si l'utilisateur est connecté, définissez isSignedIn à true, sinon à false.
+
+  //    // Exemple simplifié :
+  //    const userIsAuthenticated =
+  //    /* logique d'authentification */;
+
+  //    setIsSignedIn(userIsAuthenticated);
+  //  }, []); // Le tableau vide [] signifie que cette fonction useEffect s'exécute une seule fois après le rendu initial.
+
   return (
     <Stack.Navigator initialRouteName={Home}>
       <Stack.Screen name="Home" component={HomeTopTabs} />
       <Stack.Screen name="Spots" component={Spots} />
       <Stack.Screen name="Login" component={LoginPage} />
+      <Stack.Screen name="SignUp" component={SignUpPage} />
       <Stack.Screen name="Details" component={SpotPage} />
       <Stack.Screen name="Map" component={Map} />
     </Stack.Navigator>
