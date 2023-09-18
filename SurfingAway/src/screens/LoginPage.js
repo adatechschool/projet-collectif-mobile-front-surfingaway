f238000afe3a88d01f1ab261026c73b8d346c7ae
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-09-18 16:12:05 +0200

    Création page login

--- SurfingAway/src/screens/LoginPage.js
+++ SurfingAway/src/screens/LoginPage.js
@@ -1,11 +1,90 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { Button, Card, TextInput } from "react-native-paper";
 
 const LoginPage = () => {
-    return (
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const styles = StyleSheet.create()
+  const handleLogin = () => {
+    // Gérer la connexion ici
+  };
 
-export default LoginPage
+  const handleForgotPassword = () => {
+    // Gérer la réinitialisation du mot de passe ici
+  };
+  const handleRegistration = () => {
+    // Gérer la réinitialisation du mot de passe ici
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.mainTitle}>Rejoins la commu 🤙 </Text>
+      </View>
+      <Card style={styles.card}>
+        <Card.Content>
+          <TextInput
+            label="Email"
+            mode="outlined"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            label="Mot de passe"
+            mode="outlined"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry
+          />
+        </Card.Content>
+        <Card.Actions style={styles.actions}>
+          <Button
+            mode="outlined"
+            onPress={handleLogin}
+            style={styles.actionsContent}
+          >
+            Se connecter
+          </Button>
+          <Text style={styles.actionsContent} onPress={handleForgotPassword}>
+            Mot de passe oublié ?
+          </Text>
+          <Text style={styles.actionsContent} onPress={handleRegistration}>
+            S'inscrire
+          </Text>
+        </Card.Actions>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  card: {
+    margin: 20,
+    padding: 5,
+  },
+  titleWrapper: {
+    backgroundColor: "deeppink",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  mainTitle: {
+    fontWeight: "bold",
+    fontSize: 25,
+    color: "white",
+    paddingVertical: 10,
+  },
+  actions: {
+    flexDirection: "column",
+    padding: 15,
+  },
+  actionsContent: {
+    margin: 5,
+    fontSize: 15,
+  },
+});
+
+export default LoginPage;
